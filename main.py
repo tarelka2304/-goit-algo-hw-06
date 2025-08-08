994a897f389a2f86baab73f80e101af024a6f02e
Test User <test@example.com>
2025-08-08 14:44:57 +0300

    3

--- main.py
+++ main.py
@@ -32,7 +32,9 @@ class Record:
          self.phones.append(Phone(phone))
 
     def remove_phone(self, phone):
-         self.phones = [p for p in self.phones if p.value != phone]
+         phone_obj = self.find_phone(phone)
+         if phone_obj:
+             self.phones.remove(phone_obj)
 
     def find_phone(self,phone):
          for p in self.phones:
@@ -42,11 +44,13 @@ class Record:
          return None
 
     def edit_phone(self, old_phone, new_phone):
-         for i, p in enumerate(self.phones):
-            if p.value == old_phone:
-                self.phones[i] = Phone(new_phone)
-                return True
-         return False
+         old = self.find_phone(old_phone)
+         if not old:
+             raise ValueError("Старий номер не знайдено")
+         
+         self.remove_phone(old_phone)
+         self.add_phone(new_phone)
+         return True
          
 
     def __str__(self):
@@ -56,10 +60,12 @@ class AddressBook(UserDict):
     def add_record(self,record):
         self.data[record.name.value]=record
 
-    def find_record(self, name):
+    def find(self, name):
         return self.data.get(name)
 
-    def delete_record(self, name):
+    def delete(self, name):
         if name in self.data:
             del self.data[name]
-                
+    
+    def __str__(self):
+        return "\n".join(str(record) for record in self.data.values())
